361c4214931fc8327530be62aa1972d645f39967
MohamedNufais5891 <HUSSAIR@Dell.com>
2023-04-01 20:58:13 -1200

    database function changes

--- backend/test/test.js
+++ backend/test/test.js
@@ -94,7 +94,7 @@ describe("Teacher Endpoints", () => {
       expect(element).toHaveProperty('id');
     });
 
-    expect(body.length).toBe(2);
+    expect(body.length).toBe(3);
 
     expect(body).toContainEqual({
       "id": 10001,
@@ -141,7 +141,7 @@ describe("Student Endpoints", () => {
     expect(res.status).toEqual(200);
     let body = res.body;
 
-    expect(body.length).toBe(3)
+    expect(body.length).toBe(1)
 
     expect(body).toContainEqual({
       "id": 99999,
@@ -163,6 +163,8 @@ describe("Student Endpoints", () => {
     const res = await requestWithSupertest.get("/listStudents");
     expect(res.status).toEqual(200);
     let body = res.body;
+
+    expect(body.length).toBe(1)
 
     expect(body).toContainEqual({
       "id": 20002,
@@ -187,7 +189,7 @@ describe("Student Endpoints", () => {
     expect(res.status).toEqual(200);
     let body = res.body;
 
-    expect(body.length).toBe(3)
+    expect(body.length).toBe(1)
 
     body.forEach(element => {
       expect(element).toHaveProperty('age');
