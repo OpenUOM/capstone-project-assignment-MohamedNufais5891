7b33daa094f84240c65596f4005fec747af451b2
MohamedNufais5891 <HUSSAIR@Dell.com>
2023-04-05 23:54:54 -1200

    database function changes

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,15 @@
 import { listen } from "./server.js";
 
-if(process.env.NODE_ENV === "test"){
-  listen(3000, () => {
-    console.log(
-      "Capstone Project Backend is running on http://localhost:3000"
-    );
-  });
-  }else{
+//if(process.env.NODE_ENV === "test"){
+  //listen(3000, () => {
+    //console.log(
+      //"Capstone Project Backend is running on http://localhost:3000"
+   // );
+  //});
+ // }else{
   listen(8080, () => {
     console.log(
       "Capstone Project Backend is running on http://localhost:8080"
     );
   });
-}
+//}
