a3e38c740b9b1f8e70708fd9c0cde58a47535597
Mohamed Nufais <125565667+MohamedNufais5891@users.noreply.github.com>
2024-12-31 02:51:07 +0530

    Update student-delete.test.js

--- frontend/test/student-delete.test.js
+++ frontend/test/student-delete.test.js
@@ -5,12 +5,12 @@ fixture`Testing Student UI`
     .page`http://localhost:4401/student`
 
 test('Testing delete students', async t => {
-    await t.navigateTo("/addStudent");
+    await t.navigateTo("/deleteStudent");
     await t.typeText("#student-id", "222222");
     await t.typeText("#student-name", "Hiruni Gajanayake");
     await t.typeText("#student-age", "45");
     await t.typeText("#student-Hometown", "buddhist");
-    await t.click("#student-add");
+    await t.click("#student-delete");
 
     await t.navigateTo("/student");
 
