fed6bbe11bfe9e2c013d01a91ea96f623787c4d4
Mohamed Nufais <125565667+MohamedNufais5891@users.noreply.github.com>
2024-12-31 13:55:53 +0530

    Update test.js

--- backend/test/test.js
+++ backend/test/test.js
@@ -45,7 +45,7 @@ describe("Teacher Endpoints", () => {
     expect(res.status).toEqual(200);
     let body = res.body;
 
-    expect(body.length).toBe(3)
+    expect(body.length).toBe(4)
 
     expect(body).toContainEqual({
       "id": 10033,
