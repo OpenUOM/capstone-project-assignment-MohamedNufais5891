c4786bdeeb377b82272c2f6f8b2c4ab7178f5845
Mohamed Nufais <125565667+MohamedNufais5891@users.noreply.github.com>
2024-12-31 17:59:12 +0530

    Update test.js

--- backend/test/test.js
+++ backend/test/test.js
@@ -5,13 +5,12 @@ const requestWithSupertest = supertest(server);
 const db = require("../db-config");
 const testBase = require("./testBase");
 
+// Reset the database before all tests
 beforeAll(async () => {
   await testBase.resetDatabase(db);
 });
 
-/**
- * Reset the database after every test case
- */
+// Reset the database after every test
 afterEach(async () => {
   await testBase.resetDatabase(db);
 });
@@ -19,93 +18,79 @@ afterEach(async () => {
 describe("Teacher Endpoints", () => {
   it("GET /listTeachers should show all teachers", async () => {
     const res = await requestWithSupertest.get("/listTeachers");
-    expect(res.status).toEqual(200);
-    let body = res.body;
-    expect(body.length).toEqual(3);
-    body.forEach(element => {
-      expect(element).toHaveProperty('age');
-      expect(element).toHaveProperty('name');
-      expect(element).toHaveProperty('id');
+    expect(res.status).toBe(200);
+
+    const body = res.body;
+    expect(body.length).toBe(3);
+
+    body.forEach((teacher) => {
+      expect(teacher).toHaveProperty("id");
+      expect(teacher).toHaveProperty("name");
+      expect(teacher).toHaveProperty("age");
     });
 
-    expect(body[0].name).toBe('Kusuma Ranasinghe');
-    expect(body[1].name).toBe('Saman De Silva');
-    expect(body[2].name).toBe('Parasanna Mahagamage');
+    expect(body[0].name).toBe("Kusuma Ranasinghe");
+    expect(body[1].name).toBe("Saman De Silva");
+    expect(body[2].name).toBe("Parasanna Mahagamage");
   });
 
   it("POST /addTeacher should show a newly added teacher", async () => {
-    // add new teacher
     await requestWithSupertest.post("/addTeacher").send({
-      "id": 10033,
-      "name": "Nilanthi Fernando",
-      "age": 42
+      id: 10033,
+      name: "Nilanthi Fernando",
+      age: 42,
     });
 
     const res = await requestWithSupertest.get("/listTeachers");
-    expect(res.status).toEqual(200);
-    let body = res.body;
+    expect(res.status).toBe(200);
 
-    expect(body.length).toBe(4)
+    const body = res.body;
+    expect(body.length).toBe(4);
 
     expect(body).toContainEqual({
-      "id": 10033,
-      "name": "Nilanthi Fernando",
-      "age": 42
+      id: 10033,
+      name: "Nilanthi Fernando",
+      age: 42,
     });
   });
 
-  if("POST /editTeacher should show a newly added teacher", async () => {
-    // add new teacher
+  it("POST /editTeacher should edit a teacher", async () => {
     await requestWithSupertest.post("/editTeacher").send({
-      "id": 10002,
-      "name": "Saman",
-      "age": 50
+      id: 10002,
+      name: "Saman",
+      age: 50,
     });
 
     const res = await requestWithSupertest.get("/listTeachers");
-    expect(res.status).toEqual(200);
-    let body = res.body;
+    expect(res.status).toBe(200);
 
+    const body = res.body;
     expect(body).toContainEqual({
-      "id": 10002,
-      "name": "Saman",
-      "age": 50
+      id: 10002,
+      name: "Saman",
+      age: 50,
     });
 
     expect(body).not.toContainEqual({
-      "name": "Saman De Silva",
+      name: "Saman De Silva",
     });
   });
 
   it("POST /deleteTeacher should delete a teacher", async () => {
-
-    // delete Student
     await requestWithSupertest.post("/deleteTeacher").send({
-      "id": 10003
+      id: 10003,
     });
 
     const res = await requestWithSupertest.get("/listTeachers");
-    expect(res.status).toEqual(200);
-    let body = res.body;
+    expect(res.status).toBe(200);
 
-    body.forEach(element => {
-      expect(element).toHaveProperty('age');
-      expect(element).toHaveProperty('name');
-      expect(element).toHaveProperty('id');
-    });
-
+    const body = res.body;
     expect(body.length).toBe(2);
 
-    expect(body).toContainEqual({
-      "id": 10001,
-      "name": "Kusuma Ranasinghe",
-      "age": 45
-    });
-
     expect(body).not.toContainEqual({
-      "id": 10003,
-      "name": "Parasanna Mahagamage",
-      "age": 30
+      id: 10003,
+      name: "Parasanna Mahagamage",
+      age: 30,
     });
   });
 });
@@ -113,97 +98,87 @@ describe("Teacher Endpoints", () => {
 describe("Student Endpoints", () => {
   it("GET /listStudents should show all students", async () => {
     const res = await requestWithSupertest.get("/listStudents");
-    expect(res.status).toEqual(200);
-    let body = res.body;
-    expect(body.length).toBe(1);
-    body.forEach(element => {
-      expect(element).toHaveProperty('age');
-      expect(element).toHaveProperty('name');
-      expect(element).toHaveProperty('id');
-      expect(element).toHaveProperty('hometown');
+    expect(res.status).toBe(200);
+
+    const body = res.body;
+    expect(body.length).toBe(3);
+
+    body.forEach((student) => {
+      expect(student).toHaveProperty("id");
+      expect(student).toHaveProperty("name");
+      expect(student).toHaveProperty("age");
+      expect(student).toHaveProperty("hometown");
     });
 
-    expect(body[0].name).toBe('Sandakan');
-    expect(body[1].name).toBe('Sandun Perera');
-    expect(body[2].name).toBe('Isuri De Silva');
+    expect(body[0].name).toBe("Sandakan");
+    expect(body[1].name).toBe("Sandun Perera");
+    expect(body[2].name).toBe("Isuri De Silva");
   });
 
-  if("POST /addStudent should show a newly added student", async () => {
-    // add new student
+  it("POST /addStudent should show a newly added student", async () => {
     await requestWithSupertest.post("/addStudent").send({
-      "id": 99999,
-      "name": "Rashini Shehara",
-      "age": 12,
-      "hometown": "Galle"
+      id: 99999,
+      name: "Rashini Shehara",
+      age: 12,
+      hometown: "Galle",
     });
 
     const res = await requestWithSupertest.get("/listStudents");
-    expect(res.status).toEqual(200);
-    let body = res.body;
+    expect(res.status).toBe(200);
 
-    expect(body.length).toBe(4)
+    const body = res.body;
+    expect(body.length).toBe(4);
 
     expect(body).toContainEqual({
-      "id": 99999,
-      "name": "Rashini Shehara",
-      "age": 12,
-      "hometown": "Galle"
+      id: 99999,
+      name: "Rashini Shehara",
+      age: 12,
+      hometown: "Galle",
     });
   });
 
-  it("POST /editStudent should edit a Student", async () => {
-    // edit new student
+  it("POST /editStudent should edit a student", async () => {
     await requestWithSupertest.post("/editStudent").send({
-      "id": 20002,
-      "name": "Sandakan",
-      "age": 15,
-      "hometown": "Homagama"
+      id: 20002,
+      name: "Sandakan",
+      age: 15,
+      hometown: "Homagama",
     });
 
     const res = await requestWithSupertest.get("/listStudents");
-    expect(res.status).toEqual(200);
-    let body = res.body;
+    expect(res.status).toBe(200);
 
+    const body = res.body;
     expect(body).toContainEqual({
-      "id": 20002,
-      "name": "Sandakan",
-      "age": 15,
-      "hometown": "Homagama"
+      id: 20002,
+      name: "Sandakan",
+      age: 15,
+      hometown: "Homagama",
     });
 
     expect(body).not.toContainEqual({
-      "name": "Sandun Perera",
+      name: "Sandun Perera",
     });
   });
 
   it("POST /deleteStudent should delete a student", async () => {
-
-    // delete Student
     await requestWithSupertest.post("/deleteStudent").send({
-      "id": 20003
+      id: 20003,
     });
 
     const res = await requestWithSupertest.get("/listStudents");
-    expect(res.status).toEqual(200);
-    let body = res.body;
+    expect(res.status).toBe(200);
 
-    expect(body.length).toBe(1)
+    const body = res.body;
+    expect(body.length).toBe(2);
 
-    body.forEach(element => {
-      expect(element).toHaveProperty('age');
-      expect(element).toHaveProperty('name');
-      expect(element).toHaveProperty('id');
-      expect(element).toHaveProperty('hometown');
+    expect(body).not.toContainEqual({
+      id: 20003,
+      name: "Isuri De Silva",
+      age: 13,
+      hometown: "Galle",
     });
-
-    expect(body).toContainEqual({
-      "id": 99999,
-      "name": "Rashini Shehara",
-      "age": 12,
-      "hometown": "Galle"
-    });
-
-    
-    
   });
 });
+    
+    
