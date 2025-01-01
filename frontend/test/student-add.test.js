54623797328f5d5977b273a42be4c18d5cf00fb8
Mohamed Nufais <125565667+MohamedNufais5891@users.noreply.github.com>
2025-01-01 19:04:24 +0530

    Update student-add.test.js

--- frontend/test/student-add.test.js
+++ frontend/test/student-add.test.js
@@ -5,14 +5,14 @@ fixture`Testing Student UI`
     .page`http://localhost:4401/student`
 
 test('Testing add students', async t => {
-    await t.navigateTo("/dbinitialize");
+    await t.navigateTo("/Student");
     await t.typeText("#student-id", "999999");
     await t.typeText("#student-name", "Pasindu Basnayaka");
     await t.typeText("#student-age", "45");
     await t.typeText("#student-Hometown", "Catholic");
     await t.click("#student-add");
 
-    await t.navigateTo("/dbinitialize");
+    await t.navigateTo("/Student");
 
     const table = Selector('#student-table')
     const rowCount = await table.find('tr').count;
