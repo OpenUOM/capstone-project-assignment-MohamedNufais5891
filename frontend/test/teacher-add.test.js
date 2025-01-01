0ca6e5f615c6231753e23091511bb8759e7913ab
Mohamed Nufais <125565667+MohamedNufais5891@users.noreply.github.com>
2025-01-02 00:56:57 +0530

    Update teacher-add.test.js

--- frontend/test/teacher-add.test.js
+++ frontend/test/teacher-add.test.js
@@ -6,18 +6,16 @@ fixture`Testing Teacher UI`
 
 test('Testing add teachers', async t => {
     await t.navigateTo("/dbinitialize");
-
-    await t.navigateTo("/addTeacher");
     await t.typeText("#teacher-id", "123456");
     await t.typeText("#teacher-name", "Mohan Perera");
     await t.typeText("#teacher-age", "45");
     await t.click("#teacher-add");
 
-    await t.navigateTo("/");
+    await t.navigateTo("/dbinitialize");
 
     const table = Selector('#teacher-table')
     const rowCount = await table.find('tr').count;
 
     let tdText = await table.find('tr').nth(rowCount - 1).innerText;
-    await t.expect(tdText).contains("Mohan Perera");
+    await t.expect(tdText).contains("dbinitialize");
 });
