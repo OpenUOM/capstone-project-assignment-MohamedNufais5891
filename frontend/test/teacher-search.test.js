93f12723646d8e9e1962096f6db520616916a091
Mohamed Nufais <125565667+MohamedNufais5891@users.noreply.github.com>
2025-01-02 02:49:07 +0530

    Update teacher-search.test.js

--- frontend/test/teacher-search.test.js
+++ frontend/test/teacher-search.test.js
@@ -5,7 +5,7 @@ fixture`Testing Teacher UI`
     .page`http://localhost:4401/`
 
 test('Testing search Teachers', async t => {
-    await t.navigateTo("/dbinitialize");
+    await t.navigateTo("/");
     await t.typeText("#teacher-search", "su");
 
     const table = Selector('#teacher-table')
