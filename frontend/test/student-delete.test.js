7516c26e1f74a785a9cdf4e7886a986643fdaf34
Mohamed Nufais <125565667+MohamedNufais5891@users.noreply.github.com>
2025-01-02 02:44:35 +0530

    Update student-delete.test.js

--- frontend/test/student-delete.test.js
+++ frontend/test/student-delete.test.js
@@ -5,14 +5,14 @@ fixture`Testing Student UI`
     .page`http://localhost:4401/student`
 
 test('Testing delete students', async t => {
-    await t.navigateTo("/dbinitialize");
+    await t.navigateTo("/addStudent");
     await t.typeText("#student-id", "222222");
     await t.typeText("#student-name", "Hiruni Gajanayake");
     await t.typeText("#student-age", "45");
     await t.typeText("#student-Hometown", "buddhist");
-    await t.click("#student-delete");
+    await t.click("#student-add");
 
-    await t.navigateTo("/dbinitialize");
+    await t.navigateTo("/student");
 
     await t.click("#student-delete-222222");
 
@@ -20,5 +20,5 @@ test('Testing delete students', async t => {
     const rowCount = await table.find('tr').count;
 
     let tdText = await table.find('tr').nth(rowCount - 1).innerText;
-    await t.expect(tdText).notContains("dbinitialize");
+    await t.expect(tdText).notContains("Hiruni Gajanayake");
 });
