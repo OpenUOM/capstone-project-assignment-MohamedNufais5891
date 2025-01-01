0fb17f65aa3dc34e77ed91be69dd7dba2acec86a
Mohamed Nufais <125565667+MohamedNufais5891@users.noreply.github.com>
2025-01-01 19:07:09 +0530

    Update teacher-delete.test.js

--- frontend/test/teacher-delete.test.js
+++ frontend/test/teacher-delete.test.js
@@ -5,13 +5,13 @@ fixture`Testing Teacher UI`
     .page`http://localhost:4401/`
 
 test('Testing delete teachers', async t => {
-    await t.navigateTo("/deleteTeacher");
+    await t.navigateTo("/Teacher");
     await t.typeText("#teacher-id", "300000");
     await t.typeText("#teacher-name", "Hasitha Fernando");
     await t.typeText("#teacher-age", "45");
     await t.click("#teacher-delete");
 
-    await t.navigateTo("/teacher");
+    await t.navigateTo("/Teacher");
 
     await t.click("#teacher-delete-300000");
 
